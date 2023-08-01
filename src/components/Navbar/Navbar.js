3bcd0d739ee65313caa4a5fc683a3fd63debcb1a
Moisés Lemos <93021767+moiseslemosz@users.noreply.github.com>
2023-07-31 21:56:34 -0300

    Initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./navbar.css";
+
+export const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <span>React Kanban</span>
+      </nav>
+    </>
+  );
+};
